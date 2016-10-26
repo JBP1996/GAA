c7331a588fdad5c77a9a20bb391a8c758f6f1e9f
UserPL135Pc13 <UserPL135Pc13@SLFRPL135PC13.ensino.atec.local>
2016-10-26 12:06:16 +0100

    atualizacao 2 - mudar para classes e metodos

---
+++ src/ExercicioBalanco.java
@@ -0,0 +1,36 @@
+
+public class ExercicioBalanco {
+
+	public ExercicioBalanco() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Ola Turma!");	
+		System.out.println("Receitas: "+calcularReceitas()+"€");
+		System.out.println("Despesas: "+calcularDespesas()+"€");
+		System.out.println("Balanço: "+calcularBalanco()+"€");
+	}
+
+	public int calcularReceitas(){
+		// TODO Auto-generated method stub
+		int 	quotas = 300,
+				recBaile = 200,
+				recBar = 250;
+		int receitas = quotas + recBaile + recBar;
+		return receitas;
+	}
+	
+	public int calcularDespesas() {
+		// TODO Auto-generated method stub
+		int 	agua = 80,
+				luz = 60,
+				telefone = 70;
+		int despesas = agua + luz + telefone;
+		return despesas;
+	}
+	
+	public int calcularBalanco(){
+		// TODO Auto-generated method stub
+		int balanco = calcularReceitas() - calcularDespesas();
+		return balanco;
+	}
+
+}
--- src/Principal.java
+++ src/Principal.java
@@ -7,20 +7,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("Ola Turma!");
-		
-		int 	quotas = 300,
-				recBaile = 200,
-				recBar = 250;
-		int totalReceitas = quotas + recBaile + recBar;
-		int 	agua = 80,
-				luz = 60,
-				telefone = 70;
-		int despesas = agua + luz + telefone;
-		System.out.println("Receitas: "+totalReceitas+"€");
-		System.out.println("Despesas: "+despesas+"€");
-		System.out.println("Balanço: "+(totalReceitas - despesas)+"€");
+		new ExercicioBalanco();
 	}
 
 }
