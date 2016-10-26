8afcf242c07bcb31d6c480b6429d1c3e288da6a9
UserPL135Pc13 <UserPL135Pc13@SLFRPL135PC13.ensino.atec.local>
2016-10-26 11:34:16 +0100

    atualizacao 1

--- src/Principal.java
+++ src/Principal.java
@@ -9,6 +9,19 @@ public class Principal {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Ola Turma!");
+		
+		int 	quotas = 300,
+				recBaile = 200,
+				recBar = 250;
+		int totalReceitas = quotas + recBaile + recBar;
+		System.out.println("Receitas: "+totalReceitas+"€");
+		
+		int 	agua = 80,
+				luz = 60,
+				telefone = 70;
+		int despesas = agua + luz + telefone;
+		System.out.println("Despesas: "+despesas+"€");
+		System.out.println("Balanço: "+(totalReceitas - despesas)+"€");
 	}
 
 }
